0547a4b425da2956e5a82277b658ffc10045173b
Beeka <beekayt274@gmail.com>
2024-10-17 00:47:02 +1300

    summit

--- site/summit/index.js
+++ site/summit/index.js
@@ -1,5 +1,8 @@
 window.onload = () => {
-    console.log("wsdfsd")
+    setTimeout(()=>{document.getElementById("parallax").scrollTo(0, document.getElementById("body").scrollHeight - 1)}, 10)
+    window.onresize()
+}
+
+window.onresize = () => {
     document.body.style.setProperty("--height", document.getElementById("body").scrollHeight + "px")
-    window.scrollTo(0, document.getElementById("body").scrollHeight);
 }
