cf97cb6c5832505b19cedb1a81af1d763e85fe6c
Beeka <beekayt274@gmail.com>
2025-01-24 02:12:21 +1300

    pass 1

---
+++ site/geometry/script.js
@@ -0,0 +1,19 @@
+// https://www.youtube.com/watch?v=L1eu737bu70
+
+document.addEventListener("DOMContentLoaded", function(){
+    scrollingVideo(document.querySelector('#slopeyslopes'))
+    scrollingVideo(document.querySelector('#thechallenge'))
+    scrollingVideo(document.querySelector('#thirtyfour'))
+    scrollingVideo(document.querySelector('#thirtyfive'))
+});
+
+function scrollingVideo(section) {
+    var video = section.querySelector('video');
+    video.pause()
+    window.addEventListener('scroll', ()=>{
+        var percentage = (window.scrollY - section.offsetTop + window.innerHeight) / (section.clientHeight + window.innerHeight)
+        if (percentage > 0 && percentage < 1) {
+            video.currentTime = video.duration * percentage
+        }
+    })
+}
