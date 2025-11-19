18fafea7d06079b20c2c44ec350602e9eb6a000b
Apia <beekayt274@gmail.com>
2025-11-19 16:37:32 +1300

    chords

---
+++ site/chords/chords.js
@@ -0,0 +1,39 @@
+// how many chords are there?
+var chords = [];
+for (let i = 0; i <= 12; i++) chords.push([]);
+
+function synonyms(chord) {
+    let result = [chord];
+    for (let i = 0; i < 11; i++) {
+        chord <<= 1;
+        if (chord >= 4096) {
+            chord -= 4095; // -2^12, +2^0
+        }
+        if (!result.includes(chord)) result.push(chord);
+    }
+    result.sort((a,b)=>a-b);
+    return result;
+}
+
+for (let i = 0; i < 4096; i++) {
+    let syns = synonyms(i);
+    if (i == syns[0]) {
+        let notes = 0;
+        for (let p = 0; p < 12; p++) {
+            if (i & 1 << p) notes++;
+        }
+        chords[notes].push(syns[0]);
+    }
+}
+console.log(chords);
+
+function describe(chord, root=0) {
+    const notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+    let result = [];
+    for (let p = 0; p < 12; p++) {
+        if (chord & 1 << p) {
+            result.push(notes[(root+p) % 12]);
+        }
+    }
+    return result.join(", ")
+}
